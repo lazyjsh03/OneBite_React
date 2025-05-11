9101a164ba7911bb307d6f63d642d87fd15a8af0
lazyJSH <lazy.jsh03@gmail.com>
2025-05-11 21:39:07 +0900

    section04 complete

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,8 +1,12 @@
 // import mul from "./math.js";
 // import {add, sub} from "./math.js";
 import mul, {add, sub} from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(2, 3));
-console.log(sub(5, 3));
-console.log(mul(2, 3));
+// console.log(add(2, 3));
+// console.log(sub(5, 3));
+// console.log(mul(2, 3));
 // // index.js module
+
+const color = randomColor();
+console.log(color);
