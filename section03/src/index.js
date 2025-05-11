be921fce96ff6e0a3e7ea77f238006658227cdcc
lazyJSH <lazy.jsh03@gmail.com>
2025-05-11 20:41:58 +0900

    Install randomcolor library

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, node.js!");
+// import mul from "./math.js";
+// import {add, sub} from "./math.js";
+import mul, {add, sub} from "./math.js";
+
+console.log(add(2, 3));
+console.log(sub(5, 3));
+console.log(mul(2, 3));
+// // index.js module
