1f538d587b53cf61e4f8366b805204034de5e496
lazyJSH <lazy.jsh03@gmail.com>
2025-05-19 20:39:44 +0900

    5.10

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 회원가입 폼
 // 1. name
@@ -14,17 +14,30 @@ const Register = () => {
     bio: "",
   });
 
+  const countRef = useRef(0); // useRef = 렌더링에 영향을 끼치지 않는 변수수
+  const inputRef = useRef();
+
   const onChange = (e) => {
+    countRef.current++;
+    console.log(countRef.current);
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
   };
 
+  const onSubmit = () => {
+    if (input.name === "") {
+      // 이름을 입력하는 DOM 요소에 포커스
+      inputRef.current.focus();
+    }
+  };
+
   return (
     <div>
       <div>
         <input
+          ref={inputRef}
           name="name"
           value={input.name}
           onChange={onChange}
@@ -51,6 +64,7 @@ const Register = () => {
       <div>
         <textarea name="bio" value={input.bio} onChange={onChange} />
       </div>
+      <button onClick={onSubmit}>submit</button>
     </div>
   );
 };
